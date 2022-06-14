8081c9d6abbe56c117ea3765acde43ab36495193
Volodymyr Zdota <zdotavv@ukr.net>
2022-06-15 01:20:23 +0300

    Added security and role

--- src/main/java/com/zdota/dronesspringboot/util/config/SecurityConfig.java
+++ src/main/java/com/zdota/dronesspringboot/util/config/SecurityConfig.java
@@ -23,5 +23,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().disable();
     }
-
 }
